fb78c92e58165cdeaa1b022484df88ed190bf368
Bogusława Wolska <boguslawa.wolska@sgcib.com>
2018-05-07 22:29:41 +0200

    Changed Write to WriteLine

--- DrugaAplikacjaKonsolowa/DrugaAplikacjaKonsolowa/Program.cs
+++ DrugaAplikacjaKonsolowa/DrugaAplikacjaKonsolowa/Program.cs
@@ -10,7 +10,7 @@ namespace DrugaAplikacjaKonsolowa
     {
         static void Main(string[] args)
         {
-            Console.Write("Nacisnij dowolny klawisz aby zamknac aplikacje...");
+            Console.WriteLine("Nacisnij dowolny klawisz aby zamknac aplikacje...");
             Console.ReadKey();
         }
     }
