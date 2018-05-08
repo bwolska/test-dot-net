4fc9b7b30fc7dea5f452add18df7912843b06db5
bwolska <bm.wolska@upcpoczta.pl>
2018-05-08 12:01:44 +0200

    Calculator with test

---
+++ ConsoleApp/ConsoleApp.Test1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ConsoleApp;
+using ConsoleApp1;
+
+namespace ConsoleApp.Test1
+{
+    public class Calculator_tests
+    {
+        [Fact]
+        public void Adding_2_positive_integers()
+        {
+            // arrange
+            var x = 5;
+            var y = 10;
+            var calc = new ExampleCalculator();
+
+            // act
+            var result = calc.Add(x, y);
+
+            // assert
+            Assert.Equal(15, result);
+        }
+    }
+}
--- ConsoleApp/ConsoleApp1/Program.cs
+++ ConsoleApp/ConsoleApp1/Program.cs
@@ -11,7 +11,25 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Witamy");
-         
+            var x = int.Parse(Console.ReadLine());
+            var y = int.Parse(Console.ReadLine());
+
+            var calc = new ExampleCalculator();
+            var result = calc.Add(x, y);
+
+            Console.WriteLine(result);
+        }
+    }
+
+    public class ExampleCalculator
+    {
+        public ExampleCalculator()
+        {
+        }
+
+        public object Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
