a4306442833bf102aa85e872b94e20cc390733c3
bwolska <bm.wolska@upcpoczta.pl>
2018-05-08 16:40:03 +0200

    Selenium test2

---
+++ ClassLibrary/ClassLibrary1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HelloWeb
+{
+
+    public class HelloWebTests: IDisposable
+    {
+        private readonly string BlogUrl = "https://autotestdotnet.wordpress.com/";
+        private IWebDriver browser;
+
+        public HelloWebTests()
+        {
+            browser = new ChromeDriver();
+        }
+
+        [Fact]
+        public void Can_open_blog_and_hello_note_exits()
+        {
+ 
+            browser.Navigate().GoToUrl(BlogUrl);
+            var article = browser.FindElement(By.Id("post-3096"));
+            var header = article.FindElement(By.TagName("h1"));
+            Assert.Equal("Witamy na warsztatach automatyzacji testów!", header.Text);
+
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                browser.Quit();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
